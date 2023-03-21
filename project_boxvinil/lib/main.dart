fde1d85e8136774004196a55718d760966c012d6
Abinoã Menezes <abinoamenezes89@gmail.com>
2023-03-21 13:07:15 -0300

    pequenas alterações no main

--- project_boxvinil/lib/main.dart
+++ project_boxvinil/lib/main.dart
@@ -5,9 +5,10 @@ import './paginas/cadastro.dart';
 import './paginas/configuracoes.dart';
 import './paginas/login.dart';
 import './paginas/perfil.dart';
-import './paginas/playlist.dart';
+import 'paginas/home.dart';
 import './paginas/carregamento.dart';
 import './paginas/lista.dart';
+import './paginas/playlist.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,10 +35,11 @@ class MyApp extends StatelessWidget {
         '/': (context) => const Carregamento(),
         '/cadastro': (context) => const TelaCadastro(),
         '/login': (context) => const TelaLogin(),
-        '/playlist': (context) => const TelaPlaylist(),
+        '/home': (context) => const TelaHome(),
         '/perfil': (context) => const TelaPerfil(),
         '/configuracao': (context) => const TelaConfiguracao(),
         '/lista': (context) => const TelaLista(),
+        '/playlist': (context) => const TelaPlaylist(),
       },
     );
   }
